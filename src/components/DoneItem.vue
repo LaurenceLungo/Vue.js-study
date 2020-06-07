4cd2f7ad0422bace95cabba8bbca8c1f66f5866d
LaurenceLungo <pnng@connect.ust.hk>
2020-06-07 15:14:06 +0800

    initial commit

---
+++ src/components/DoneItem.vue
@@ -0,0 +1,33 @@
+<template>
+    <div class="done-item">
+        <p>
+            {{done.title}}
+            <button @click="$emit('del-done', done.id)" class="del">x</button>
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "DoneItem",
+    props: ["done"]
+}
+</script>
+
+<style scoped>
+    .done-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .del {
+        background: #ff0000;
+        color: #ffff;
+        border: none;
+        padding: 5px 9px;
+        border-radius: 50%;
+        cursor: pointer;
+        float: right;
+    }
+</style>
---
+++ src/components/Dones.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <div v-bind:key="done.id" v-for="done in dones">
+            <DoneItem v-bind:done="done" v-on:del-done="$emit('del-done', done.id)" />
+        </div>
+    </div>
+</template>
+
+<script>
+import DoneItem from './DoneItem';
+export default {
+    name: "Dones",
+    components: {
+        DoneItem
+    },
+    props: ["dones"]
+}
+</script>
+
+<style scoped>
+
+</style>
